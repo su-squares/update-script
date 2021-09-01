6a70ac2d2a159519c19b2d503604cc97c9a8f6cc
William Entriken <github.com@phor.net>
2021-09-01 18:03:05 -0400

    Make everything synchronous

--- image-processing.js
+++ image-processing.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const fs = require("fs");
 const sharp = require('sharp');
+
+const SQUARE_EDGE_PIXELS = 10;
+const NUM_SQUARES_ON_EDGE = 100;
 
 var basefilename = null;
 var composites = [];
@@ -10,31 +14,37 @@ function loadMillionPixelBase(newFilename) {
 };
 
 function markSquareAsNotPersonalized(squareNumber) {
-  const zeroBasedColumn = (squareNumber - 1) % 100;
-  const zeroBasedRow = Math.floor((squareNumber - 1) / 100);
+  const zeroBasedColumn = (squareNumber - 1) % NUM_SQUARES_ON_EDGE;
+  const zeroBasedRow = Math.floor((squareNumber - 1) / NUM_SQUARES_ON_EDGE);
 
   composites.push({
     input: 'assets/one-gray-square.png',
-    left: 10 * zeroBasedColumn,
-    top: 10 * zeroBasedRow,
+    left: SQUARE_EDGE_PIXELS * zeroBasedColumn,
+    top: SQUARE_EDGE_PIXELS * zeroBasedRow,
   });
 }
 
 function overlaySquarePersonalization(squareNumber, rgbData) {
-  const zeroBasedColumn = (squareNumber - 1) % 100;
-  const zeroBasedRow = Math.floor((squareNumber - 1) / 100);
+  const zeroBasedColumn = (squareNumber - 1) % NUM_SQUARES_ON_EDGE;
+  const zeroBasedRow = Math.floor((squareNumber - 1) / NUM_SQUARES_ON_EDGE);
 
   composites.push({
     input: rgbData,
-    raw: {width: 10, height: 10, channels: 3},
-    left: 10 * zeroBasedColumn,
-    top: 10 * zeroBasedRow,
+    raw: {width: SQUARE_EDGE_PIXELS, height: SQUARE_EDGE_PIXELS, channels: 3},
+    left: SQUARE_EDGE_PIXELS * zeroBasedColumn,
+    top: SQUARE_EDGE_PIXELS * zeroBasedRow,
   });
 }
 
 function saveImage(outputFilename) {
-  return sharp(basefilename)
-    .composite(composites)
+  const inputBuffer = fs.readFileSync(basefilename);
+
+  if (composites.length > 0) { // https://github.com/lovell/sharp/issues/2872
+    return sharp(inputBuffer)
+      .composite(composites)
+      .toFile(outputFilename);
+  }
+  return sharp(inputBuffer)
     .toFile(outputFilename);
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const { ethers } = require("ethers");
+const chalk = require("chalk");
 const config = require("./config.json");
 const imageProcessing = require("./image-processing");
 const { start } = require("repl");
 const progress = fs.existsSync("./build/progress.json")
     ? require("./build/progress.json")
-    : { };
+    : null; 
 
 const provider = new ethers.providers.JsonRpcProvider(config.provider);
 const suSquaresABI = [
@@ -27,70 +28,88 @@ var allData = Array(NUM_SQUARES).fill(null);
 var background = "./assets/empty-board.png";
 var startBlock = config.startBlock;
 
-if (progress.progress) {
-    console.log("Resuming from " + progress.progress)
+if (progress) {
+    console.log(chalk.blue("Resuming from:        ") + progress.processedBlock);
     background = "./build/wholeSquare.png";
-    startBlock = progress.progress;
+    startBlock = progress.processedBlock;
 }
 
 imageProcessing.loadMillionPixelBase(background);
 
-// Which squares were touched? /////////////////////////////////////////////////
-provider.getBlockNumber().then(blockNumber => {
-    endBlock = startBlock + 50000; //------------------------------------------------------------TESTING THIS LINE
-    console.log("Loading from " + startBlock + " to " + endBlock);
+// Main program, synchronous ///////////////////////////////////////////////////
+(async function() {
+    // Which Squares to process? ///////////////////////////////////////////////
+    // Set up loading range
+    const currentBlock = await provider.getBlockNumber();
+    console.log(chalk.blue("Current block:        ") + currentBlock);
 
-    // Personalized...
+    // How many blocks to process?
+    const numberOfBlocksToProcess = parseInt(process.argv[process.argv.length - 1])
+        ? parseInt(process.argv[process.argv.length - 1])
+        : 1;
+    const endBlock = startBlock + numberOfBlocksToProcess;
+    console.log(chalk.blue("Loading to:           ") + endBlock);
+
+    // Which personalized...
     const filterPersonalized = suSquaresContract.filters.Personalized();
-    const personalized = suSquaresContract.queryFilter(filterPersonalized, startBlock, endBlock).then(events => {
+    const personalized = await suSquaresContract.queryFilter(filterPersonalized, startBlock, endBlock).then(events => {
         return events.map(event => event.args.squareNumber);
     });
-
-    // Transferred...
+    console.log(chalk.blue("Personalized Squares: ") + personalized);
+    
+    // Which were transferred from the contract (i.e. "minted")...
     const filterTransferFromContract = suSquaresContract.filters.Transfer(config.account, null, null);
-    const transferFromContract = suSquaresContract.queryFilter(filterTransferFromContract, startBlock, endBlock).then(events => {
+    const transferFromContract = await suSquaresContract.queryFilter(filterTransferFromContract, startBlock, endBlock).then(events => {
         return events.map(event => event.args.squareNumber);
     });
+    console.log(chalk.blue("Transferred Squares:  ") + transferFromContract);
+    
+    const touchedSquares = [...new Set([].concat(personalized, transferFromContract))];
+    touchedSquares.sort();
+    console.log(chalk.blue("Touched:              ") + touchedSquares);
+    console.log(chalk.yellow("NUMBER OF SQUARES TOUCHED: ") + touchedSquares.length + "\n\n");
 
-    let touchedSquares = Promise.all([personalized, transferFromContract]).then(arrayOfArrayOfSquares => {
-        return new Set([].concat(...arrayOfArrayOfSquares));
-    });
-
-    // Collect details on touched squares //////////////////////////////////////
-    let squareProcessing = touchedSquares.then(touchedSquares => {
-        return Promise.all([...touchedSquares].map(squareNumber => {
-            console.log("Processing square:", squareNumber.toString());
-
-            return suSquaresContract.suSquares(squareNumber).then(result => {
-                console.log('got square ' + squareNumber.toString());
-                // Save to the master JSON file
-                allData[squareNumber - 1] = [result.version, result.title, result.href];
-        
-                let paddedSquareNumber = ("00000" + squareNumber).slice(-5);
-                // Build ERC-721 metadata JSON file
-                var erc721Object = {
-                "name" : "Square #" + paddedSquareNumber,
-                "description" : result ? result[2] : "",
-                "image" : "https://tenthousandsu.com/erc721/" + paddedSquareNumber + ".png"
-                };
-                fs.writeFileSync("build/metadata/" + paddedSquareNumber + ".json", JSON.stringify(erc721Object));
-        
-                // If the square is personalized then save image data
-                if (result && result.rgbData && result.rgbData.length === (2 + 300*2)) {
-                    const rgbData = Buffer.from(result.rgbData.substr(2), "hex");
-                    imageProcessing.overlaySquarePersonalization(squareNumber, rgbData);
-                } else {
-                    imageProcessing.markSquareAsNotPersonalized(squareNumber);
-                }
-            });
-        }));
-    });
-
-    // Exit when complete
-    squareProcessing.then((x) => {
-        imageProcessing.saveImage("build/wholeSquare.png");
-        fs.writeFileSync("build/progress.json", JSON.stringify( { progress: endBlock + 1 }));
-        fs.writeFileSync("build/squarePersonalizations.json", JSON.stringify(allData));
+    if (touchedSquares.length > 500) {
+        console.log(chalk.red("TOO MANY SQUARES TO PROCESS"));
         process.exit();
-    });        
-});
+    }
+
+    // Launch per-Square processing jobs (in parallel) /////////////////////////
+    let squareProcessing = Promise.all(touchedSquares.map(squareNumber => {
+        // console.log(chalk.blue("Processing Square:    ") + squareNumber.toString());
+
+        return suSquaresContract.suSquares(squareNumber).then(result => {
+            const paddedSquareNumber = ("00000" + squareNumber).slice(-5);
+            console.log(chalk.green("Got blockchain state for Square: ") + squareNumber.toString());
+            
+            // Save for squarePersonalizations.json
+            allData[squareNumber - 1] = [result.version, result.title, result.href];
+    
+            // Build ERC-721 metadata JSON file
+            const erc721Object = {
+                "name": "Square #" + paddedSquareNumber,
+                "description": result.title ? result.title : "",
+                "image": "https://tenthousandsu.com/erc721/" + paddedSquareNumber + ".png"
+            };
+            fs.writeFileSync("build/metadata/" + paddedSquareNumber + ".json", JSON.stringify(erc721Object));
+    
+            // If the square is personalized then save image data
+            if (result.rgbData.length === (2 + 300*2)) {
+                const rgbData = Buffer.from(result.rgbData.substr(2), "hex");
+                imageProcessing.overlaySquarePersonalization(squareNumber, rgbData);
+            } else {
+                imageProcessing.markSquareAsNotPersonalized(squareNumber);
+            }
+        });
+    }));
+
+    // Wait for all to complete ////////////////////////////////////////////////
+    squareProcessing.then(() => {
+        imageProcessing.saveImage("build/wholeSquare.png");
+        fs.writeFileSync("build/squarePersonalizations.json", JSON.stringify(allData));
+        fs.writeFileSync("build/progress.json", JSON.stringify({processedBlock: endBlock}));
+
+        // TODO: Print time timestamp and ISO 8601
+        process.exit();
+    });    
+}());
